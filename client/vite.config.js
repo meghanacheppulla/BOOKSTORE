4be388929655d7716fcb31c6e1e5bb33b1c80249
naveenkumar6788 <147487898+naveenkumar6788@users.noreply.github.com>
2026-07-07 20:57:33 +0530

    server
    Added server port configuration to Vite setup.

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
+// server port
 export default defineConfig({
   plugins: [react()],
   server: {
